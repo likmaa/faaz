280d8b270eb7b11a7ed19ebcb07037b804b22197
ABDOUL MALIK AKOGO <125963279+likmaa@users.noreply.github.com>
2026-06-27 16:42:20 +0100

    fix: resolve linter warnings in config/urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include, re_path
-from django.conf import settings
-from django.views.static import serve
+from django.contrib import admin  # type: ignore
+from django.urls import path, include, re_path  # type: ignore
+from django.conf import settings  # type: ignore
+from django.views.static import serve  # type: ignore
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
